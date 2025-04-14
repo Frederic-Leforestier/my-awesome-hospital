7af35980779f4ac9fba4ba3c575023e9126c0981
GregoryLef <gregorylefrancois.travail@yahoo.com>
2025-04-14 14:11:58 +0200

    Class js base finie

---
+++ js/class/doctor.js
@@ -0,0 +1,10 @@
+class Doctors {
+    constructor(first, last, id, birth, speciality) {
+        this.firstName = first;
+        this.lastName = last;
+        this.rpps = id;
+        this.birthDay = birth;
+        this.appointement = [];
+        this.speciality = speciality;
+    }
+};
